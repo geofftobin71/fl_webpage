566000bb0be45203aec51a6666653068e5b8a7d0
play.js <git@gituser.com>
2020-08-07 11:36:02 +1200

    basic functional blog

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,13 @@
 module.exports = function (config) {
+  
+  config.addCollection("posts", function (collection) {
+    return collection.getFilteredByGlob("src/site/posts/*.md");
+  });
+
   return {
     dir: {
       input: "src/site",
+      includes: "_includes",
       output: "_site",
     },
   };
---
+++ src/site/posts/my-third-post.md
@@ -0,0 +1,9 @@
+---
+title: "This is my third post"
+date: "2019-08-27"
+layout: layouts/base.njk
+---
+
+# {{ title }}
+
+This is my third paragraph.
