ebf7b8f29287fdecceb0aae96969727e418c2f6f
play.js <git@gituser.com>
2020-08-07 22:14:54 +1200

    update folders

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,7 @@
 module.exports = function (config) {
   
   config.addCollection("posts", function (collection) {
-    return collection.getFilteredByGlob("src/site/posts/*.md");
+    return collection.getFilteredByGlob("posts/*.md");
   });
 
-  return {
-    dir: {
-      input: "src/site",
-      includes: "_includes",
-      output: "_site",
-    },
-  };
 };
