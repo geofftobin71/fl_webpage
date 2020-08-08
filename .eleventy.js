232b59ca8e1cf61479fe9117eb032aac47769925
play.js <git@gituser.com>
2020-08-08 17:20:08 +1200

    instagram feed

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,6 @@ module.exports = function (config) {
   config.addCollection("posts", function (collection) {
     return collection.getFilteredByGlob("posts/*.md");
   });
-
+  
   config.addPassthroughCopy("css");
 };
---
+++ _data/instagram.js
@@ -0,0 +1,8 @@
+const fetch = require('node-fetch');
+
+module.exports = async function() {
+
+  return fetch('https://graph.instagram.com/me/media?fields=media_url,caption&access_token=IGQVJWMlZANa0RmREZAKVWZAhLVg5UG9ZAVlhhYV9CRzlQNTZAwQ01tSVdSeVMydzVqbWtVQk5RbnpGQ3FpaWZAWRUtVM1lPbGtJT1M2N0YycWVlbjNZAUkZAIdUNvZATdRakJfUEhodU9McVR5V0diMG1MZA3hmeQZDZD')
+  .then(res => res.json())
+  .then(json => { return json.data; });
+};
