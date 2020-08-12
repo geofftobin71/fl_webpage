2708440cb689de3f02c956c7fde4f6965b620b58
play.js <git@gituser.com>
2020-08-12 17:53:31 +1200

    fonts

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const { DateTime } = require("luxon"); 
+
 module.exports = function (config) {
   
   config.addCollection("posts", function (collection) {
@@ -5,4 +7,23 @@ module.exports = function (config) {
   });
   
   config.addPassthroughCopy("css");
+  
+  config.addFilter("readableDate", dateObj => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
+  });
+
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  config.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+  });
+
+  // Get the first `n` elements of a collection.
+  config.addFilter("head", (array, n) => {
+    if( n < 0 ) {
+      return array.slice(n);
+    }
+
+    return array.slice(0, n);
+  });
+
 };
