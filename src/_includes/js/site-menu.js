c7a93493486327fce54afad6fa8232f55b8d80ae
geofftobin71 <geofftobin71@gmail.com>
2020-12-31 22:02:51 +0000

    site menu

--- src/_includes/js/site-menu.js
+++ src/_includes/js/site-menu.js
@@ -1,10 +1,21 @@
-var prevScrollPos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollPos >= currentScrollPos) {
-    document.getElementById("site-menu").style.top = "0";
-  } else {
-    document.getElementById("site-menu").style.top = "-8rem";
-  }
-  prevScrollPos = currentScrollPos > 0 ? currentScrollPos : 0;
-}
+const site_menu = document.querySelector("#site-menu");
+const header = document.querySelector("#header");
+
+const site_menu_options = { rootMargin: "-70px 0px 0px 0px" };
+
+const site_menu_observer = new IntersectionObserver((entries, site_menu_observer) => {
+  entries.forEach(entry => {
+    if(!entry.isIntersecting) {
+      site_menu.classList.add("opaque");
+      site_menu.classList.remove("link-lighter");
+      // site_menu.classList.add("link-darker");
+    }
+    else {
+      site_menu.classList.remove("opaque");
+      site_menu.classList.add("link-lighter");
+      // site_menu.classList.remove("link-darker");
+    }
+  });
+}, site_menu_options);
+
+site_menu_observer.observe(header);
