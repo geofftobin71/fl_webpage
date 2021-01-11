6ee72ca262367abf9cd7b596c8e636b53ac191c5
geofftobin71 <geofftobin71@gmail.com>
2021-01-11 02:47:58 +0000

    instagram gallery

--- src/_data/instagram_gallery.js
+++ src/_data/instagram_gallery.js
@@ -1,6 +1,7 @@
-const fetch = require('node-fetch');
 const fs = require('fs');
+const fetch = require('node-fetch');
 const sharp = require('sharp');
+const murmurhash = require('murmurhash');
 
 async function getImage(item, index) {
   let newItem = {};
@@ -10,7 +11,8 @@ async function getImage(item, index) {
   const response = await fetch(newItem.source);
   const buffer = await response.buffer();
   const path = '/images/instagram/';
-  const filename = 'floriade_wellington_' + ('0000' + (index + 1)).slice(-4) + '.jpg';
+  // const filename = 'floriade_wellington_' + ('0000' + (index + 1)).slice(-4) + '.jpg';
+  const filename = 'floriade_wellington_' + murmurhash.v3(newItem.source) + '.jpg';
 
   newItem.image = path + filename;
   newItem.thumbnail = path + 'thumbs/' + filename;
@@ -31,40 +33,16 @@ async function getImage(item, index) {
     .toBuffer()
     .then((data) => {
       newItem.preview = "data:image/jpeg;base64," + data.toString('base64');
-      console.log(JSON.stringify(newItem, null, 2));
+      // if(item.media_type == 'VIDEO') { newItem.image = item.media_url; }
+      // console.log(JSON.stringify(newItem, null, 2));
       return newItem;
     });
 };
 
-const getImages = async (json_data) => {
-  const cache_path = 'dist/cache/';
-  const cache_file = cache_path + 'instagram_cache.json';
-
-  if(fs.existsSync(cache_file))
-  {
-    fs.readFile(cache_file, 'utf8', (err, contents) => {
-      if(err) throw err;
-
-      var cache_data = JSON.parse(contents);
-
-      var i;
-      for(i = 0; i < json_data.length; ++i) {
-        let json_media = json_data[i].thumbnail_url ? json_data[i].thumbnail_url : json_data[i].media_url;
-        let cache_media = cache_data[i].source;
-
-        if(json_media != cache_media) { cache_data = []; break; }
-      }
-
-      if(cache_data.length) { return cache_data; }
-    });
-  }
-
-  fs.mkdirSync('dist/images/instagram/thumbs/', { recursive: true });
-
+const getImages = async (json_data, cache_file) => {
   return Promise.all(json_data.map((item, index) => getImage(item, index)))
     .then((result) => {
-      console.log(JSON.stringify(result, null, 2));
-      fs.mkdirSync(cache_path, { recursive: true });
+      // console.log(JSON.stringify(result, null, 2));
       fs.writeFileSync(cache_file, JSON.stringify(result, null, 2));
       console.log('Cached ' + cache_file);
 
@@ -78,7 +56,38 @@ module.exports = function() {
   // .then(res => res.json())
   // .then(json => console.log(json));
 
-  return fetch('https://graph.instagram.com/me/media?fields=media_url,thumbnail_url,caption&access_token=' + process.env.INSTAGRAM_TOKEN)
+  return fetch('https://graph.instagram.com/me/media?fields=media_type,media_url,thumbnail_url,caption&access_token=' + process.env.INSTAGRAM_TOKEN)
     .then(res => res.json())
-    .then(json => { return getImages(json.data); });
+    .then(json => { 
+      const cache_path = 'cache/';
+      const cache_file = cache_path + 'instagram_cache.json';
+
+      if(fs.existsSync(cache_file))
+      {
+        const contents = fs.readFileSync(cache_file, 'utf8');
+
+        let cache_data = JSON.parse(contents);
+
+        if(json.data.length == cache_data.length)
+        {
+          let i;
+          for(i = 0; i < json.data.length; ++i) {
+            let json_media = json.data[i].thumbnail_url ? json.data[i].thumbnail_url : json.data[i].media_url;
+            let cache_media = cache_data[i].source;
+
+            if(json_media != cache_media) { cache_data = []; break; }
+          }
+        }
+
+        if(cache_data.length) {
+          console.log("Instagram cache up to date.");
+          return cache_data;
+        }
+      }
+
+      fs.mkdirSync(cache_path, { recursive: true });
+      fs.mkdirSync('dist/images/instagram/thumbs/', { recursive: true });
+
+      return getImages(json.data, cache_file);
+    });
 };
