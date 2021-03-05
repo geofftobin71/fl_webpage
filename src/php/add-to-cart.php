9358af4ec6a82e61ff679e66ec1ad7e668d40746
geofftobin71 <geofftobin71@gmail.com>
2021-03-05 06:06:08 +0000

    add to cart

--- src/php/add-to-cart.php
+++ src/php/add-to-cart.php
@@ -1,4 +1,4 @@
-<?php include $_SERVER['DOCUMENT_ROOT'] . '/php/shop-functions.php'; ?>
+<?php include $_SERVER["DOCUMENT_ROOT"] . "/php/shop-functions.php"; ?>
 
 <?php
 
@@ -24,19 +24,29 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
   $product_count = intval(clean($_POST["product_count"]));
   $product_count = ($product_count < 1) ? 1 : $product_count;
 
+  $_SESSION["product_id"] = $product_id;
+  $_SESSION["variant_id"] = $variant_id;
+  $_SESSION["product_count"] = $product_count;
+
   if(isFinite($product_id, $variant_id)) {
     $stock_count = stockCount($product_id, $variant_id);
 
     if($product_count > $stock_count) {
-      header("Location:" . $return_url . "?error=Number must be less than or equal to " . $stock_count . "#add-to-cart-form");
+      $_SESSION["product_count"] = $stock_count;
+      $_SESSION["error"] = "Number must be less than or equal to " . $stock_count;
+      header("Location:" . $return_url . "#add-to-cart-form");
     }
   }
 
-  echo 'Product ID: ' . $_POST["product_id"] . '<br>';
-  echo 'Variant ID: ' . $_POST["variant_id"] . '<br>';
-  echo 'Product count: ' . $_POST["product_count"] . '<br>';
-  echo 'Return URL: ' . $_POST["return_url"] . '<br>';
+  echo "Product ID: " . $_POST["product_id"] . "<br>";
+  echo "Variant ID: " . $_POST["variant_id"] . "<br>";
+  echo "Product count: " . $_POST["product_count"] . "<br>";
+  echo "Return URL: " . $_POST["return_url"] . "<br>";
   
+  unset($_SESSION["product_id"]);
+  unset($_SESSION["variant_id"]);
+  unset($_SESSION["product_count"]);
+  unset($_SESSION["error"]);
 }
 
 ?>
--- src/php/shop-functions.php
+++ src/php/shop-functions.php
@@ -2,6 +2,9 @@
 header('Cache-Control: no-cache, no-store, must-revalidate');
 header('Pragma: no-cache');
 header('Expires: 0');
+session_start();
+
+if(!isset($_SESSION['cart'])) { $_SESSION['cart']=array(); }
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/../php/sleekdb-config.php';
 
