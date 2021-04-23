20e596e16b9094db3567872e5ef8a44feef72cbf
geofftobin71 <geofftobin71@gmail.com>
2021-04-23 03:57:46 +0000

    new layout css

--- src/_includes/js/shop-cart.js
+++ src/_includes/js/shop-cart.js
@@ -55,7 +55,7 @@ async function displayCart() {
 	
 	  cart_items += '</p>';
 	
-	  cart_items += '<div class="font-base font-size--1" style="display:flex;justify-content:flex-start">';
+	  cart_items += '<div class="horizontal left font-base font-size--1">';
 	  cart_items += '<div class="icon-button color-shade3" onclick="removeFromCart(' + i + ')">';
 	  cart_items += '<span><svg viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M704 736v576q0 14-9 23t-23 9h-64q-14 0-23-9t-9-23v-576q0-14 9-23t23-9h64q14 0 23 9t9 23zm256 0v576q0 14-9 23t-23 9h-64q-14 0-23-9t-9-23v-576q0-14 9-23t23-9h64q14 0 23 9t9 23zm256 0v576q0 14-9 23t-23 9h-64q-14 0-23-9t-9-23v-576q0-14 9-23t23-9h64q14 0 23 9t9 23zm128 724v-948h-896v948q0 22 7 40.5t14.5 27 10.5 8.5h832q3 0 10.5-8.5t14.5-27 7-40.5zm-672-1076h448l-48-117q-7-9-17-11h-317q-10 2-17 11zm928 32v64q0 14-9 23t-23 9h-96v948q0 83-47 143.5t-113 60.5h-832q-66 0-113-58.5t-47-141.5v-952h-96q-14 0-23-9t-9-23v-64q0-14 9-23t23-9h309l70-167q15-37 54-63t79-26h320q40 0 79 26t54 63l70 167h309q14 0 23 9t9 23z"/></svg></span><p class="text-lowercase">Remove</p>';
 	  cart_items += '</div>';
