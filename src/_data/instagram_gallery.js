4796ae9907c1d488775c683c2470c33f7e6af63c
geofftobin71 <geofftobin71@gmail.com>
2021-05-19 05:50:38 +0000

    test vs live

--- src/_data/instagram_gallery.js
+++ src/_data/instagram_gallery.js
@@ -11,7 +11,7 @@ async function getImage(item) {
       resource_type: 'image',
       overwrite: false,
       async: false,
-      context: 'caption=' + item.caption + '|timestamp=' + item.timestamp
+      context: 'caption=' + encodeURIComponent(item.caption) + '|timestamp=' + item.timestamp
     }, 
     function(error, result) { /* console.log(result); */ }));
 };
@@ -43,7 +43,7 @@ module.exports = function() {
             .max_results(500)
             .execute()
             .then(result => {
-              // console.log(JSON.stringify(result.resources, null, 2));
+              console.log(JSON.stringify(result.resources, null, 2));
               if(result && result.resources && result.resources.length) {
                 if(process.env.NODE_ENV == 'build') {
                   console.log('Updating instagram-gallery');
