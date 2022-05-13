7959504f5e7928372d2effafc14d676008b23dc1
geofftobin71 <geofftobin71@gmail.com>
2022-05-13 22:54:22 +0000

    wedding couples

--- .eleventy.js
+++ .eleventy.js
@@ -304,6 +304,15 @@ module.exports = (eleventyConfig) => {
     return content;
   });
 
+  eleventyConfig.addFilter("addNbsp", (str) => {
+    if (!str) {
+      return;
+    }
+    let title = str.replace(/((.*)\s(.*))$/g, "$2&nbsp;$3");
+    title = title.replace(/"(.*)"/g, '\\"$1\\"');
+    return title;
+  });
+
   eleventyConfig.addFilter("uniqueId", (length) => {
     return uniqueId(length);
   });
